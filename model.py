61d18d72a5cdc437c1db20ddf8196d7a3bc31dd8
hlin863 <55798830+hlin863@users.noreply.github.com>
2022-02-03 15:15:00 +0000

    Uploaded program
    Program is code using the SIR Model and generated sample output of susceptible, infected, and recovered population based on a random population of 1000.

---
+++ model.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({'font.size': 22})
+
+beta, gamma = 0.9, 0.4
+N = 1000
+t_end = 30
+
+Rs = []
+ts = []
+Is = []
+Ss = []
+CIs = []
+
+for i in range(3):
+    R = [0]
+    t = [0]
+    I = [10]
+    S = [N - I[0]]
+    CI = [0]
+
+    j = 0
+
+    while I[j] > 0 and t[j] < t_end:
+        a = beta * S[j] * I[j] / N
+        b = gamma * I[j]
+
+        p_i = a / (a + b)
+        p_r = b / (a + b)
+
+        u1 = np.random.uniform(0, 1)
+        u2 = np.random.uniform(0, 1)
+
+        if 0 < u1 <= p_i:
+            S.append(S[j] - 1)
+            I.append(I[j] + 1)
+            CI.append(R[j] + I[j])
+            # R.append(R[int(t[j])])
+            R.append(R[j])
+        elif p_i < u1 < 1:
+            S.append(S[j])
+            I.append(I[j] - 1)
+            CI.append(R[j] + I[j])
+            # R.append(R[int(t[j])] + 1)
+            R.append(R[j] + 1)
+        
+        if S[j] + I[j] + R[j] != N:
+                print('Population: ' + str(S[j] + I[j] + R[j]))
+        
+        t.append(t[j] - np.log(u2) / (a + b))
+        j += 1
+    
+    Rs.append(R)
+    ts.append(t)
+    Is.append(I)
+    Ss.append(S)
+    CIs.append(CI)
+
+plt.title("SIR Model - infected")
+plt.xlabel("time")
+plt.ylabel("population")
+plt.plot(ts[0], Is[0], color='r' , label='Infected 1')
+plt.plot(ts[0], Rs[0], color='g', label='Recovered 1')
+plt.plot(ts[0], Ss[0], color='b', label='Susceptible 1')
+plt.plot(ts[0], CIs[0], color='k', label='Cumulative infected 1')
+
+# plt.plot(ts[0], Is[0] + Rs[0] + Ss[0], color='k', label='Total 1')
+# plt.plot(ts[1], Is[1], color='r', label='Infected 2')
+# plt.plot(ts[2], Is[2], color='g', label='Infected 3')
+plt.legend()
+plt.show()
