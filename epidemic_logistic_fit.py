073a3e4815fccc2d08cbdc671f152f02e2320938
hlin863 <55798830+hlin863@users.noreply.github.com>
2022-02-28 19:24:24 +0000

    Update epidemic_logistic_fit.py
    Completed solution to simulate the data based on sample at different time intervals

--- epidemic_logistic_fit.py
+++ epidemic_logistic_fit.py
@@ -1,3 +1,4 @@
+from turtle import color
 import warnings
 import numpy as np # import numpy
 import matplotlib.pyplot as plt # library to plot the data
@@ -62,6 +63,53 @@ print(sample_time) # print the sample time
 print(sample_infected) # print the sample data
 
 plt.plot(sample_time, sample_infected, color = 'k') # plot the sample data
+plt.scatter(sample_time, sample_infected, color = 'r') # plot the individual data points
+
+# split the sample_data into 5 sections to plot 5 lines
+sample_infected_1 = sample_infected[0:int(len(sample_infected)/5)] # first section
+sample_time_1 = sample_time[0:int(len(sample_time)/5)] # first section
+
+sample_infected_2 = sample_infected[0:int(len(sample_infected)/5)*2] # second section
+sample_time_2 = sample_time[0:int(len(sample_time)/5)*2] # second section
+
+sample_infected_3 = sample_infected[0:int(len(sample_infected)/5)*3] # third section
+sample_time_3 = sample_time[0:int(len(sample_time)/5)*3] # third section
+
+sample_infected_4 = sample_infected[0:int(len(sample_infected)/5)*4] # fourth section
+sample_time_4 = sample_time[0:int(len(sample_time)/5)*4] # fourth section
+
+sample_infected_5 = sample_infected[0:int(len(sample_infected))] # fifth section
+sample_time_5 = sample_time[0:int(len(sample_time))] # fifth section
+
+def sample_simulation(sample_t, sample_infected, fit_function): # function to perform simulation on the samples.
+
+    sample_output = [] # initialize sample_output
+
+    popt, pcov = curve_fit(fit_function, sample_t, sample_infected) # perform curve fitting on the sample data
+
+    a_f = float(popt[0]) # a_f is the fitted parameter a
+    b_f = float(popt[1]) # b_f is the fitted parameter b
+    c_f = float(popt[2]) # c_f is the fitted parameter c
+
+    for i in range(0, len(sample_t)): # iterate through the sample data
+        sample_output.append(fit_function(sample_t[i], a_f, b_f, c_f)) # simulates the data
+    
+    return sample_output
+
+sample_1_output = sample_simulation(sample_time_1, sample_infected_1, logistic_fit) # perform simulation on the first section
+sample_2_output = sample_simulation(sample_time_2, sample_infected_2, logistic_fit) # perform simulation on the second section
+sample_3_output = sample_simulation(sample_time_3, sample_infected_3, logistic_fit) # perform simulation on the third section
+sample_4_output = sample_simulation(sample_time_4, sample_infected_4, logistic_fit) # perform simulation on the fourth section
+sample_5_output = sample_simulation(sample_time_5, sample_infected_5, logistic_fit) # perform simulation on the fifth section
+
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k'] # colors for the lines
+
+plt.plot(sample_time_1, sample_1_output, color = colors[0]) # plot the first section
+plt.plot(sample_time_2, sample_2_output, color = colors[1]) # plot the second section
+plt.plot(sample_time_3, sample_3_output, color = colors[2]) # plot the third section
+plt.plot(sample_time_4, sample_4_output, color = colors[3]) # plot the fourth section
+plt.plot(sample_time_5, sample_5_output, color = colors[4]) # plot the fifth section
+
 plt.xlabel('Time (days)') # x-axis label
 plt.ylabel('Cumulative Infected Population') # y-axis label
 plt.title('Cumulative Infected Population vs. Time') # title
